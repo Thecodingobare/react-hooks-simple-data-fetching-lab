5ea49b5f8aa698c57a298f7a696932c86b1a6345
Thecodingobare <daniel.obare@student.moringaschool.com>
2022-07-15 13:40:03 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  if (!data.length) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={data} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
